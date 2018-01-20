bffefa95cd373002d939317f7668234f6e6b4962
MArco A de Campos <titocampos@gmail.com>
2018-01-20 19:34:44 -0200

    Adding to repository

--- app/src/main/java/com/mecatronica/ring/AbstractActivity.java
+++ app/src/main/java/com/mecatronica/ring/AbstractActivity.java
@@ -6,5 +6,8 @@ public abstract class AbstractActivity extends AppCompatActivity {
     public static final int REQUEST_ENABLE_BT = 1;
     public static final String ACTION_CONNECTED =
             "com.mecatronica.ring.action.CONNECTED";
+    public static final String ACTION_CONNECTION_FAILURE =
+            "com.mecatronica.ring.action.FAILURE";
+
 
 }
